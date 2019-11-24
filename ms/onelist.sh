32dc4e03e8ec26eaccbf7b99b1e7299342df88ce
caippx <37325838+caippx@users.noreply.github.com>
2019-11-24 17:55:58 +0800

    Update onelist.sh

--- ms/onelist.sh
+++ ms/onelist.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 apt update -y
-apt install python3 python3-pip -y
+apt install python3 python3-pip git -y
 pip3 install tornado
+git clone https://github.com/MoeClub/OneList.git && cd OneList
 echo "
 请打开
 https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=ea2b36f6-b8ad-40be-bc0f-e5e4a4a7d4fa&response_type=code&redirect_uri=http://localhost/onedrive-login&response_mode=query&scope=offline_access%20User.Read%20Files.ReadWrite.All
