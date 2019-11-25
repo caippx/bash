28f04d3d2b7dded71a89e543ede51dbc21ca054e
caippx <37325838+caippx@users.noreply.github.com>
2019-11-25 20:31:58 +0800

    Update onelist.sh

--- ms/onelist.sh
+++ ms/onelist.sh
@@ -6,7 +6,7 @@ pip3 install tornado
 git clone https://github.com/caippx/OneList.git && cd OneList
 echo "
 请打开
-https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=b51b95c2-a4b0-44de-a3c1-14f23a1d2d02&response_type=code&redirect_uri=http://localhost/onedrive-login&response_mode=query&scope=offline_access%20User.Read%20Files.ReadWrite.All
+https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=b51b95c2-a4b0-44de-a3c1-14f23a1d2d02&response_type=code&redirect_uri=https://api.us.ppxwo.top/onedrive&response_mode=query&scope=offline_access%20User.Read%20Files.ReadWrite.All
 获取code"
 echo && stty erase '^H' && read -p "输入code: " code
 python3 OneList.py << EOF
