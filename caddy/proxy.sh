9435cf10d6517e0715a944666e828a290f377a1a
caippx <37325838+caippx@users.noreply.github.com>
2019-12-25 16:32:46 +0800

    Update proxy.sh

--- caddy/proxy.sh
+++ caddy/proxy.sh
@@ -109,5 +109,5 @@ runcaddy=`ps -ef | grep -v grep | grep "caddy"`
 }
 
 
-[ ! -f "/etc/caddy/caddy.conf" ] && Caddy_Install && Caddy_Config
+[ ! -f "/etc/caddy/caddy.conf" ] && Caddy_Install
 [ -f "/etc/caddy/caddy.conf" ] && echo "Caddy已经安装 增加新反代配置" &&Caddy_Config
