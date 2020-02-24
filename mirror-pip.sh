05d9a510d3615775701b928864080ee243cb7cf5
caippx <37325838+caippx@users.noreply.github.com>
2020-02-24 16:13:53 +0800

    Update mirror-pip.sh

--- mirror-pip.sh
+++ mirror-pip.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+function centos(){
 mv /etc/yum.repos.d /etc/yum.repos.d.backup
 mkdir /etc/yum.repos.d
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
@@ -13,3 +14,50 @@ echo "
 index-url=http://mirrors.aliyun.com/pypi/simple/
 trusted-host=mirrors.aliyun.com
 " > pip.conf
+}
+
+function ubuntu16(){
+mv /etc/apt/sources.list /etc/apt/sources.list.bak
+echo "
+deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
+" > /etc/apt/sources.list
+apt update
+cd ~ && mkdir .pip
+echo "
+[global]
+index-url=http://mirrors.aliyun.com/pypi/simple/
+trusted-host=mirrors.aliyun.com
+" > pip.conf
+}
+
+function ubuntu18(){
+mv /etc/apt/sources.list /etc/apt/sources.list.bak
+echo "
+deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+" > /etc/apt/sources.list
+apt update
+cd ~ && mkdir .pip
+echo "
+[global]
+index-url=http://mirrors.aliyun.com/pypi/simple/
+trusted-host=mirrors.aliyun.com
+" > pip.conf
+}
