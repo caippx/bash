207a1bbb8a2d45084b207a102dd78f437c02cd16
caippx <37325838+caippx@users.noreply.github.com>
2020-08-05 00:05:09 +0800

    Add files via upload

---
+++ oneindex/token.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'token_type' => 'Bearer',
+  'scope' => 'Files.Read Files.Read.All Files.Read.Selected Files.ReadWrite Files.ReadWrite.All Files.ReadWrite.AppFolder Files.ReadWrite.Selected Sites.Manage.All Sites.Read.All Sites.ReadWrite.All User.Read profile openid email',
+  'expires_in' => 3599,
+  'ext_expires_in' => 3599,
+  'access_token' => 'atk888',
+  'refresh_token' => 'rtk888',
+  'expires_on' => timeon,
+);
