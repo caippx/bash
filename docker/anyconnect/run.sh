3e582a5a1b8c9de6be480e9c11e7e6f65a7849a5
caippx <37325838+caippx@users.noreply.github.com>
2020-09-18 16:36:57 +0800

    Update run.sh

--- docker/anyconnect/run.sh
+++ docker/anyconnect/run.sh
@@ -8,7 +8,7 @@ sed -i "s/server.key.pem/key.key/" /etc/ocserv/ocserv.conf
 sed -i "s/#udp-port = 443/udp-port = 443/" /etc/ocserv/ocserv.conf
 
 openssl genrsa -out /etc/ocserv/key.key 2048
-openssl req -new -x509 -days 3650 -key /etc/ocserv/key.key -out /etc/ocserv/crt.crt -subj -subj "/C=HH/ST=HANHAN/L=HH/O=hanhanvpn/OU=hanhanvpn/CN=${domain}"
+openssl req -new -x509 -days 3650 -key /etc/ocserv/key.key -out /etc/ocserv/crt.crt -subj "/C=HH/ST=HANHAN/L=HH/O=hanhanvpn/OU=hanhanvpn/CN=${domain}"
 
 ocpasswd -g NoRoute ppxnoroute <<EOF
 $pwd
