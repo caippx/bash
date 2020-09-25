372e946d5e7a43d17c567496d75e87fc65cb8c97
caippx <37325838+caippx@users.noreply.github.com>
2020-09-25 18:07:59 +0800

    Update run.sh

--- docker/zfaka/run.sh
+++ docker/zfaka/run.sh
@@ -2,18 +2,18 @@
 
 
 #修改数据库信息
-sed -i "s/Mysql_Host/$Host/g" /data/wwwroot/zfaka/conf/application.ini
-sed -i "s/Mysql_Port/$Port/g" /data/wwwroot/zfaka/conf/application.ini
-sed -i "s/Mysql_User/$User/g" /data/wwwroot/zfaka/conf/application.ini
-sed -i "s/Mysql_Passwd/$PassWD/g" /data/wwwroot/zfaka/conf/application.ini
-sed -i "s/Mysql_DataBase/$DataBase/g" /data/wwwroot/zfaka/conf/application.ini
+sed -i "s/Mysql_Host/$HOST/g" /data/wwwroot/zfaka/conf/application.ini
+sed -i "s/Mysql_Port/$PORT/g" /data/wwwroot/zfaka/conf/application.ini
+sed -i "s/Mysql_User/$USER/g" /data/wwwroot/zfaka/conf/application.ini
+sed -i "s/Mysql_Passwd/$PASSWD/g" /data/wwwroot/zfaka/conf/application.ini
+sed -i "s/Mysql_DataBase/$DATABASE/g" /data/wwwroot/zfaka/conf/application.ini
 
 #配置虚拟主机信息
 echo "
 server {
     listen       80;
     server_name  $DOMAIN; #换成你的域名
-    
+    root /data/wwwroot/zfaka/public
     location ~ \.php$ {
             fastcgi_pass   unix:/usr/local/php/var/run/www-php-fpm.sock;
             fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
