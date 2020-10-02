e87f0491b0be029dbd3362b34c1d8bdc39673122
caippx <37325838+caippx@users.noreply.github.com>
2020-10-02 19:04:47 +0800

    Update install_panel.sh

--- drfytgybhunjoimkol/install_panel.sh
+++ drfytgybhunjoimkol/install_panel.sh
@@ -726,6 +726,9 @@ TO_743(){
 cd ~ && wget https://raw.githubusercontent.com/caippx/bash/master/drfytgybhunjoimkol/LinuxPanel-7.4.3.zip
 unzip LinuxPanel-* && cd panel && bash update.sh
 cd .. && rm -f LinuxPanel-*.zip && rm -rf panel
+echo "44.234.251.213 www.bt.cn" >>  /etc/hosts && chattr +i /etc/hosts
+latest_ver=$(curl -s https://www.bt.cn/api/panel/get_version)
+sed -i "s/7.4.3/${latest_ver}" /www/server/panel/class/common.py
 }
 
 
