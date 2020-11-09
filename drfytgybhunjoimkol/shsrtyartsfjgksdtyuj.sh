b25582b263a5ff41321d3f9e17ff8339291a2e9d
caippx <37325838+caippx@users.noreply.github.com>
2020-11-09 12:24:15 +0000

    Update shsrtyartsfjgksdtyuj.sh

--- drfytgybhunjoimkol/shsrtyartsfjgksdtyuj.sh
+++ drfytgybhunjoimkol/shsrtyartsfjgksdtyuj.sh
@@ -728,7 +728,7 @@ unzip LinuxPanel-* && cd panel && bash update.sh
 cd .. && rm -f LinuxPanel-*.zip && rm -rf panel
 echo "44.234.251.213 www.bt.cn" >>  /etc/hosts && chattr +i /etc/hosts
 sed -i "s/time.localtime(ltd)/time.localtime(7955085722)/"  /www/server/panel/BTPanel/__init__.py
-curl -s www.bt.cn
+curl -s -o /dev/null www.bt.cn
 }
 
 
