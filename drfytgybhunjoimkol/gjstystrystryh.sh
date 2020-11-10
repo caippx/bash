afc9ac6b2fe79035cd8ea1d564abfd3917bf70be
caippx <37325838+caippx@users.noreply.github.com>
2020-11-10 11:11:25 +0000

    Update gjstystrystryh.sh

--- drfytgybhunjoimkol/gjstystrystryh.sh
+++ drfytgybhunjoimkol/gjstystrystryh.sh
@@ -4,7 +4,7 @@ unzip LinuxPanel-* && cd panel && bash update.sh
 cd .. && rm -f LinuxPanel-*.zip && rm -rf panel
 #latest_ver=$(curl -s https://www.bt.cn/api/panel/get_version)
 echo "44.234.251.213 www.bt.cn" >>  /etc/hosts && chattr +i /etc/hosts
-#sed -i "s/7.4.3/${latest_ver}/" /www/server/panel/class/common.py
+sed -i "s/7.4.3/${latest_ver}/" /www/server/panel/class/common.py
 sed -i "s/time.localtime(ltd)/time.localtime(7955085722)/"  /www/server/panel/BTPanel/__init__.py
 curl -s -o /dev/null www.bt.cn
 bt restart
