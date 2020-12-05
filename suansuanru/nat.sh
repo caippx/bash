20618eb54d53ce140e2ca674080830f4e589bc29
caippx <37325838+caippx@users.noreply.github.com>
2020-12-05 03:36:27 +0000

    Update nat.sh

--- suansuanru/nat.sh
+++ suansuanru/nat.sh
@@ -6,18 +6,23 @@ wget https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-amd64
 mv gost-linux-amd64-2.11.1 /usr/bin/gost && chmod +x /usr/bin/gost
 }
 
+function list(){
+ps -ef | grep -v grep | grep "gost"
+}
+
 function run(){
 echo && stty erase '^H' && read -p "输入远程IP（域名）: " proxy_ip
 echo && stty erase '^H' && read -p "输入远程端口: " proxy_port
 echo && stty erase '^H' && read -p "输入本地端口: " local_port
-nohup gost -L=tcp://:$local_port/proxy_ip:proxy_port >>/dev/null 2>&1 &
+nohup gost -L=tcp://:$local_port/$proxy_ip:$proxy_port >>/dev/null 2>&1 &
+nohup gost -L=udp://:$local_port/$proxy_ip:$proxy_port >>/dev/null 2>&1 &
 sleep 3
 a=`ps -aux|grep $!| grep -v grep`
 [[ -n ${a} ]] && echo "启动成功！进程ID：$!"
 [[ -z ${a} ]] && echo "启动失败，请自己找错误 嘻嘻"
 }
 
-
+function gogogo(){
 if command -v gost >/dev/null 2>&1; then 
   run
 else 
@@ -25,3 +30,7 @@ else
   install_gost
   run
 fi
+}
+
+[[ $1 ==  "list" ]] && list
+[[ $1 ==  "" ]] && gogogo
