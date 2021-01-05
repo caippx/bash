cf1bf22e4977696402380250d0a7648bd1b92082
caippx <37325838+caippx@users.noreply.github.com>
2021-01-05 08:39:23 +0000

    Update install_ssh.sh

--- install_ssh.sh
+++ install_ssh.sh
@@ -5,3 +5,4 @@ apt install ssh screen zip unzip lrzsz -y
 sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 service sshd restart
+rm -rf $0
