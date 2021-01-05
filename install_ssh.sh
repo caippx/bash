594c800071db6889e2b7c07d423d5887a41da9fe
caippx <37325838+caippx@users.noreply.github.com>
2021-01-05 08:38:54 +0000

    Update install_ssh.sh

--- install_ssh.sh
+++ install_ssh.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #For_Dataideas_Debian_10_ssh
-password=$1
 apt install ssh screen zip unzip lrzsz -y
 sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
