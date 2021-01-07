3ab5862a67888ed6307b665115fe2e26489dcc3c
caippx <37325838+caippx@users.noreply.github.com>
2021-01-07 08:35:36 +0000

    Update newdtrfgyuhjihgujsrtjk.sh

--- drfytgybhunjoimkol/newdtrfgyuhjihgujsrtjk.sh
+++ drfytgybhunjoimkol/newdtrfgyuhjihgujsrtjk.sh
@@ -4,7 +4,29 @@ echo "44.234.251.213 www.bt.cn" >>  /etc/hosts && chattr +i /etc/hosts
 sed -i "s/time.localtime(ltd)/time.localtime(7955085722)/"  /www/server/panel/BTPanel/__init__.py
 curl -s -o /dev/null www.bt.cn
 wget -qO /www/server/panel/data/plugin.json http://www.bt.cn/api/panel/get_soft_list_test
+echo "True" > /www/server/panel/data/licenes.pl
+}
+
+Change_Path(){
+echo "\/$1" > /www/server/panel/data/admin_path.pl
+}
+
+Change_Admin(){
+bt << EOF
+6
+$2
+EOF
+}
+
+Change_Passwd(){
+bt << EOF
+5
+$3
+EOF
 }
 
 curl -sSO http://download.bt.cn/install/install_panel.sh && bash install_panel.sh
 Happy_Bt
+[[ -n $1 ]] && Change_Path
+[[ -n $2 ]] && Change_Admin
+[[ -n $3 ]] && Change_Passwd
