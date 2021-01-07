4dd73ebbcf2464f2875852fd2b1afde0f2cc9bc9
caippx <37325838+caippx@users.noreply.github.com>
2021-01-07 09:08:04 +0000

    Update test.sh

--- drfytgybhunjoimkol/test.sh
+++ drfytgybhunjoimkol/test.sh
@@ -2,12 +2,12 @@ function go1(){
 echo $1
 }
 function go2(){
-echo $2
+echo $1
 }
 function go3(){
-echo $3
+echo $1
 }
 
-[[ -n $1 ]] && go1
-[[ -n $2 ]] && go2
-[[ -n $3 ]] && go3
+[[ -n $1 ]] && go1 $1
+[[ -n $2 ]] && go2 $2
+[[ -n $3 ]] && go3 $3
