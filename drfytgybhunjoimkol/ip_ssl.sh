e8e47aaf6ca2544b81a2d0356358a0479b982d00
caippx <37325838+caippx@users.noreply.github.com>
2021-01-08 06:37:45 +0000

    Update ip_ssl.sh

--- drfytgybhunjoimkol/ip_ssl.sh
+++ drfytgybhunjoimkol/ip_ssl.sh
@@ -2,3 +2,14 @@
 
 #把宝塔自签ssl证书修改为免费的信任ip证书
 
+ip=`curl -s http://whatismyip.akamai.com/`
+
+ssl_dir="/www/server/panel/vhost/ssl/$ip"
+bt_ssl_dir="/www/server/panel/ssl"
+
+
+crt=`cat $ssl_dir/fullchain.pem`
+key=`cat $ssl_dir/privkey.pem`
+
+echo "$crt" > $bt_ssl_dir/certificate.pem
+echo "$key" > $bt_ssl_dir/privateKey.pem
