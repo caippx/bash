069212b1fcfd121e35ddcf48870c4409bedb74ca
caippx <37325838+caippx@users.noreply.github.com>
2021-08-19 17:06:13 +0800

    Update p2p.sh

--- APT/p2p.sh
+++ APT/p2p.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://raw.githubusercontent.com/caippx/bash/master/APT/p2pclient
 chmod +x p2pclient
-./p2pclient -l admin@ppxwo.com
+nohup ./p2pclient -l admin@ppxwo.com  >> /tmp/p2pclient.log 2>&1 &
