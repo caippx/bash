7e0993a9d9b077db8d132740f96be250074e1d0c
caippx <37325838+caippx@users.noreply.github.com>
2022-01-10 11:38:33 +0800

    Create tmpssh.sh

---
+++ tmpssh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo root:ppxwo.com |sudo chpasswd root
+sudo sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sudo service sshd restart
