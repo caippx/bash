099d6b7904795d43d92135cbce17e2f70f81a263
caippx <37325838+caippx@users.noreply.github.com>
2022-02-12 13:46:15 +0800

    Update p2p.sh

--- drfytgybhunjoimkol/p2p.sh
+++ drfytgybhunjoimkol/p2p.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 wget https://updates.peer2profit.com/p2pclient_0.56_amd64.deb
 dpkg -i p2pclient_0.56_amd64.deb
-nohup p2pclient --login admin@ppxwo.com 2>1 &
+ip=`hostname -I`
+nohup p2pclient --login admin@ppxwo.com -n "${ip};8.8.8.8" 2>1 &
