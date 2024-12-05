4b34824d35904a420bc932bfd3ea24ff863ef885
caippx <37325838+caippx@users.noreply.github.com>
2024-12-05 15:27:48 +0800

    Update buildx-ui.sh

--- suansuanru/apline/buildx-ui.sh
+++ suansuanru/apline/buildx-ui.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-apk add git 
+apk add git e2fsprogs e2fsprogs-extra
 apk add build-base
-apk add libc-dev
+apk add libc-dev 
 wget https://go.dev/dl/go1.23.3.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.23.3.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 go version
