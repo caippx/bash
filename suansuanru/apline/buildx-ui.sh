01613f55122088dce5fbf5edc125a4ea0212a8b9
caippx <37325838+caippx@users.noreply.github.com>
2024-12-05 15:20:19 +0800

    Create buildx-ui.sh

---
+++ suansuanru/apline/buildx-ui.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+apk add git 
+apk add build-base
+apk add libc-dev
+wget https://go.dev/dl/go1.23.3.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.23.3.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go version
+
+git clone https://github.com/MHSanaei/3x-ui && cd 3x-ui
+go build
