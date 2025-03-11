7cb4ed332477c137b3acbae02644db9b29756c21
caippx <37325838+caippx@users.noreply.github.com>
2025-03-11 16:36:22 +0800

    Update gost_nat.sh

--- suansuanru/gost_nat.sh
+++ suansuanru/gost_nat.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 function install_gost(){
+
+if curl --connect-timeout 5 -s https://github.com > /dev/null 2>&1; then
+    echo "能够访问 github.com"
+    # 如果访问正常，这里使用原始地址
+    GITHUB_URL="https://github.com"
+else
+    echo "无法访问 github.com，使用代理"
+    # 如果访问失败，则替换为代理地址
+    GITHUB_URL="https://ghproxy.11451185.xyz/github.com"
+fi
+
 # 我自己只用64位系统 就不判断是不是32了
 arch=$(uname -m)
 # 判断是否为 ARM 架构
 if [[ "$arch" == "arm"* || "$arch" == "aarch64" ]]; then
     echo "系统为aarch64"
-    wget https://github.com/go-gost/gost/releases/download/v3.0.0/gost_3.0.0_linux_arm64.tar.gz && tar -zxvf gost_3.0.0_linux_arm64.tar.gz
+    wget ${GITHUB_URL}/go-gost/gost/releases/download/v3.0.0/gost_3.0.0_linux_arm64.tar.gz && tar -zxvf gost_3.0.0_linux_arm64.tar.gz
     rm -rf LICENSE README.md README_en.md gost_3.0.0_linux_arm64.tar.gz
     mv gost /usr/bin/gost && chmod +x /usr/bin/gost
 else
     echo "系统为X86_64"
-    wget https://github.com/go-gost/gost/releases/download/v3.0.0/gost_3.0.0_linux_amd64.tar.gz && tar -zxvf gost_3.0.0_linux_amd64.tar.gz
+    wget ${GITHUB_URL}/go-gost/gost/releases/download/v3.0.0/gost_3.0.0_linux_amd64.tar.gz && tar -zxvf gost_3.0.0_linux_amd64.tar.gz
     rm -rf LICENSE README.md README_en.md gost_3.0.0_linux_amd64.tar.gz
     mv gost /usr/bin/gost && chmod +x /usr/bin/gost
 fi
