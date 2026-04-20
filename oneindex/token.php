085f3f824dfa75554b52450ae3da847d57e4a77b
caippx <37325838+caippx@users.noreply.github.com>
2026-04-20 11:58:55 +0800

    Delete oneindex directory

--- oneindex/token.php
+++
@@ -1,9 +0,0 @@
-<?php return array (
-  'token_type' => 'Bearer',
-  'scope' => 'Files.ReadWrite.All User.Read profile openid email',
-  'expires_in' => 3599,
-  'ext_expires_in' => 3599,
-  'access_token' => 'atk888',
-  'refresh_token' => 'rtk888',
-  'expires_on' => timeon,
-);
